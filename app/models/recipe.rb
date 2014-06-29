e3a3d891a55a68e67c077e471380df1d368a40d0
Madeline VEYRENC <madeline.veyrenc@openwide.fr>
2014-06-29 18:21:19 +0200

    Recipe show page

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,4 +2,8 @@ class Recipe < ActiveRecord::Base
   validates :name, presence: true
   validates :ingredients, presence: true
   validates :directions, presence: true
+  
+  def to_s
+    name
+  end
 end
