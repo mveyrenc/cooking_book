fb34167335206982e05fb4dfad4fbf8fc8446327
Madeline VEYRENC <madeline.veyrenc@openwide.fr>
2014-07-06 17:46:13 +0200

    Remove link to dish type show view

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,8 @@ Rails.application.routes.draw do
 
   get '/admin' => 'dish_types#index'
   
-  resources :dish_types, path: '/admin/dish_types'
+  resources :dish_types, path: '/admin/dish_types', :except => [:show]
+  resources :dish_types, path: '/admin/dish_types', :only => [:show], :defaults => { :format => 'json' }
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
