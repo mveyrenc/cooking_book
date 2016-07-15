fa7241af9a9c6b92ba4ccc3b6db0e5f7d07292c3
Madeline VEYRENC <madeline@veyrenc.fr>
2016-07-15 11:42:17 +0200

    Add french translations

--- config/application.rb
+++ config/application.rb
@@ -17,8 +17,8 @@ module CookingBook
     # config.time_zone = 'Central Time (US & Canada)'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+#     config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+     config.i18n.default_locale = :fr
   
     config.action_view.field_error_proc = Proc.new { |html_tag, instance| 
       "<div class=\"has-error\">#{html_tag}</div>".html_safe
