e4db10117d60f898bced2b1ec401f1df84f4a696
Madeline VEYRENC <madeline@veyrenc.fr>
2016-07-15 19:36:52 +0200

    Use devise_parameter_sanitizer.permit instead of devise_parameter_sanitizer.for

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,8 +8,8 @@ class ApplicationController < ActionController::Base
   before_filter :configure_permitted_parameters, if: :devise_controller?
   
     def configure_permitted_parameters
-      devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :email, :password, :password_confirmation) }
-      devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :email, :password, :password_confirmation, :current_password) }
+      devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password, :password_confirmation) }
+      devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :password_confirmation, :current_password) }
     end
   
     rescue_from CanCan::AccessDenied do |exception|
