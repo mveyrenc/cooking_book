d302445697ebb93ee8ad76a68b5d0972dd2a60eb
Madeline VEYRENC <madeline@veyrenc.fr>
2020-02-15 17:38:13 +0100

    Display 15 recipes on homepage with less infos

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,7 @@
 class WelcomeController < ApplicationController
   
   def index
-    @recipes = (can? :read, Recipe) ? Recipe.all.order( 'created_at DESC' ).limit( 12 ) : []
+    @recipes = (can? :read, Recipe) ? Recipe.all.order( 'created_at DESC' ).limit( 15 ) : []
   end
 
 end
