070a32228afff2901dbbb1ed86ee5ee1c30eabb2
Madeline VEYRENC <madeline@veyrenc.fr>
2020-04-27 17:33:29 +0200

    Add bulma card component to display recipes

---
+++ app/components/bulma/components/card/card_footer_item.rb
@@ -0,0 +1,44 @@
+module Bulma
+  module Components
+    module Card
+      class CardFooterItem < ViewComponent::Base
+        include Bulma::Component
+        include FontAwesomeHelper
+
+        def initialize(
+            url:,
+            name:,
+            icon:,
+            method: :get,
+            data: {},
+            styles: {}
+        )
+          @url = url
+          @name = name
+          @icon = icon
+          @method = method
+          @data = data
+          self.styles = {is_light: true}.merge(styles)
+        end
+
+        def call
+          link_to url, :class => css_class, :title => name, :method => method, :data => data do
+            render FontAwesome::Icon.new(icon: icon)
+          end
+        end
+
+        private
+
+        attr_reader :url
+        attr_reader :name
+        attr_reader :icon
+        attr_reader :method
+        attr_reader :data
+
+        def default_css_classes
+          ['card-footer-item no-print']
+        end
+      end
+    end
+  end
+end
