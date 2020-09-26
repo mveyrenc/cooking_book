5bd4a35463bd970888b3bb313248dad39d3b4013
Madeline VEYRENC <madeline@veyrenc.fr>
2020-09-26 19:24:17 +0200

    Move tag component directly in application/elements

---
+++ app/components/application/elements/tag_component.rb
@@ -0,0 +1,25 @@
+module Application
+  module Elements
+    class TagComponent < ViewComponent::Base
+      include Application::Component
+
+      def initialize(
+          styles: {}
+      )
+        self.styles = styles
+      end
+
+      def call
+        content_tag :span, {:class => css_class} do
+          content
+        end
+      end
+
+      private
+
+      def default_css_classes
+        ['tag']
+      end
+    end
+  end
+end
