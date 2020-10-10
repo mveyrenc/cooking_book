b0fd1ae849d2b6a875b1574a750c932455d2728c
Madeline VEYRENC <madeline@veyrenc.fr>
2020-10-10 22:40:05 +0200

    Change site color

--- app/components/main_menu/button_navbar_item_component.rb
+++ app/components/main_menu/button_navbar_item_component.rb
@@ -1,13 +1,31 @@
 module MainMenu
   class ButtonNavbarItemComponent < ViewComponent::Base
-    def initialize(title:, path:)
+
+    def initialize(title:, path:, icon: nil, color: nil)
       @title = title
       @path = path
+      @icon = icon
+      @color = color
     end
 
     private
 
     attr_reader :title
     attr_reader :path
+    attr_reader :icon
+    attr_reader :color
+
+    def css_class
+      url_string = URI.parser.unescape(url_for(path)).force_encoding(Encoding::BINARY)
+
+      css_class = ["button is-large is-rounded"]
+      unless color.nil?
+        css_class << "is-#{Application::Color::COLORS[color][:class]}"
+      end
+      if current_page?(path) or request.env['PATH_INFO'].start_with? url_string
+        css_class << "is-active"
+      end
+      css_class.join(' ')
+    end
   end
 end
